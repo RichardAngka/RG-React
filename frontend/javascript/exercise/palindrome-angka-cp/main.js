8c1c26cca0f1c760da6456928fddb54e20f0e2ad
RichardAngka <angkawinata25@gmail.com>
2022-03-31 21:09:15 +0700

    palindrome

--- frontend/javascript/exercise/palindrome-angka-cp/main.js
+++ frontend/javascript/exercise/palindrome-angka-cp/main.js
@@ -1,6 +1,6 @@
 /**
  * Carilah angka palindrome yang LEBIH BESAR dan TERDEKAT dari angka-angka dibawah ini
- * 
+ *
  * Misal:
  * - Jika angka pada parameter bernilai 90 maka angka palindrome terdekat adalah 99
  * - Jika angka pada parameter bernilai 102 maka angka palindrome terdekat adalah 111
@@ -8,10 +8,19 @@
 
 function angkaPalindrome(num) {
   // TODO: answer here
+  var num = num + 1;
+  while (true) {
+    var numString = num.toString();
+    var numReverse = numString.split("").reverse().join("");
+    if (numString === numReverse) {
+      return num;
+    }
+    num++;
+  }
 }
 
 console.log(angkaPalindrome(10)); //11
 console.log(angkaPalindrome(17)); //22
 console.log(angkaPalindrome(175)); //181
 
-module.exports = angkaPalindrome
+module.exports = angkaPalindrome;
