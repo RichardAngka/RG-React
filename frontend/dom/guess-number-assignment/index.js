00462913705397668d7b707916bd1dce8175c76c
RichardAngka <angkawinata25@gmail.com>
2022-04-06 21:48:34 +0700

    done

--- frontend/dom/guess-number-assignment/index.js
+++ frontend/dom/guess-number-assignment/index.js
@@ -2,30 +2,65 @@
 Uncomment variable dibawah ini untuk mulai mengerjakan. dilarang mengganti nama variable yang dibuat.
 */
 
-// let newGameBtn = tombol untuk melakukan restart game
-// let randomNumber = generate random number dari 1 sampai 10
-// let message = element untuk menampilkan pesan
-// let displayScore = element untuk menampilkan score
-// let input = element untuk memasukan data
-// let checkBtn = tombol untuk melakuan pengecekan angka pada input
-// let score = nilai yang akan ditampilakan
+const generateRandomNumber = () => Math.floor(Math.random() * 10 + 1);
+let newGameBtn = document.getElementsByClassName("new-game")[0];
+let checkBtn = document.querySelector(".btn-primary");
+let randomNumber = generateRandomNumber();
+let message = document.getElementsByClassName("message")[0];
+let displayScore = document.getElementsByClassName("score")[0];
+let input = document.getElementsByClassName("input")[0];
+let score = 10;
 
+newGameBtn.addEventListener("click", () => {
+  resetGame();
+});
+checkBtn.addEventListener("click", () => {
+  checkNumber();
+  displayScore.innerHTML = score;
+});
 // TODO: answer here
-
 
 function displayMessage(msg) {
   // TODO: answer here
+  message.innerText = msg;
 }
 
 function resetGame() {
   // TODO: answer here
+  score = 10;
+  displayScore.innerHTML = score;
+  displayMessage("Lets start guessing...");
+  input.value = "";
+  randomNumber = generateRandomNumber();
   //dilarang menghapus code dibawah ini!
-  document.getElementById('hidden-number').innerHTML = randomNumber
+  document.getElementById("hidden-number").innerHTML = randomNumber;
 }
 
 function checkNumber() {
   // TODO: answer here
+  // kurang 1 conditional di mana dia cek kalau score habis
+  if (score > 1) {
+    if (Number(input.value) === randomNumber) {
+      displayMessage("Yeay! you guessed it!");
+    }
+    if (Number(input.value) !== randomNumber) {
+      if (Number(input.value) < 1 || Number(input.value) > 10) {
+        displayMessage("Guess any number between 1 and 10");
+        // TODO: Bener gak hayo
+        score--;
+      } else if (Number(input.value) > randomNumber) {
+        displayMessage("Oww... that's too big!");
+        score--;
+      } else if (Number(input.value) < randomNumber) {
+        displayMessage("Too small, buddy!");
+        score--;
+      }
+    }
+  } else {
+    score--;
+    displayMessage("Oops, you lost the game");
+  }
 }
 
 //dilarang menghapus code dibawah ini!
-document.getElementById('hidden-number').innerHTML = randomNumber
+document.getElementById("hidden-number").innerHTML = randomNumber;
--- frontend/dom/stopwatch-app-assignment/index.js
+++ frontend/dom/stopwatch-app-assignment/index.js
@@ -2,26 +2,72 @@
 Uncomment variable dibawah ini untuk mulai mengerjakan. dilarang mengganti nama variable yang dibuat.
 */
 
-// let stopwatch = mendapatkan value dari stopwatch
-// let startbtn = tombol untuk memulai stapwatch
-// let stopbtn = tombol untuk memberhentikan stopwatch
-// let resetbtn = tombol untuk mereset value dari stopwatch
+let stopwatch = document.getElementById("stopwatch");
+let startbtn = document.getElementById("start");
+let stopbtn = document.getElementById("stop");
+let resetbtn = document.getElementById("reset");
 
 // TODO: answer here
-
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let stopTimer = true;
+let timeOut;
+startbtn.addEventListener("click", () => {
+  start();
+});
+stopbtn.addEventListener("click", () => {
+  stop();
+});
+resetbtn.addEventListener("click", () => {
+  reset();
+  stop();
+});
 
 function start() {
   // TODO: answer here
+  if (stopTimer) {
+    stopTimer = false;
+    runTime();
+  }
 }
 
 function stop() {
   // TODO: answer here
+  if (!stopTimer) {
+    stopTimer = true;
+    clearTimeout(timeOut);
+  }
 }
 
 function reset() {
   // TODO: answer here
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  stopwatch.innerText = "00:00:00";
 }
 
 function runTime() {
   // TODO: answer here
+  if (!stopTimer) {
+    seconds++;
+    if (seconds === 60) {
+      minutes++;
+      seconds = 0;
+    }
+    if (minutes === 60) {
+      hours++;
+      minutes = 0;
+      // seconds = 0
+    }
+  }
+  stopwatch.innerHTML = `${hours}:${minutes}:${seconds}`;
+  timeOut = setTimeout(runTime, 1000);
 }
+
+// Javascript
+
+// Browser
+// setTimeout
+// 1000ms => 1s
